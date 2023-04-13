b86a6f4fc7599d71dd814e2d276e755165ea8d05
Nicolas Desrosiers <nicolascarriere15@gmail.com>
2023-04-13 12:35:01 -0400

    s11c2 affichage des images dans le carrousel + modification du sass pour afficher le bouton de fermeture

--- carrousel.php
+++ carrousel.php
@@ -1,17 +1,17 @@
 <?php
 /**
  * Plugin Name: Carrousel
- * Author: Antoine Chamberland
- * Author uri: https://github.com/achamberland11
- * Description: Permet d'afficher les images d'une galerie dans une boîte modale navigable
+ * Author: Nicolas Carriere
+ * Author uri: https://github.com/nicolasDesrosiers
+ * Description: Permet d'afficher les images d'une galerie dans une boîte modale naviguable
  */
 
-    /* style.css pour formater et animer le caroussel */
-    /* carrousel.js pour contrôler le carrousel */
-    /* Boîte modale qui contiendra le carrousel */
+ /* style.css  formater et animer le carrousel */
+ /* carrousel.js  pour contrôler le carrousel */
+ /* boîte modale qui contiendra le carrousel */
 
-/*
-    filemtime() // retourne en milliseconde le temps de la dernière sauvegarde
+ /*
+ filemtime() // retourne en milliseconde le temps de la dernière sauvegarde
 plugin_dir_path() // retourne le chemin du répertoire du plugin
 __FILE__ // une constante contenant le chemin du fichier en train de s'exécuter
 wp_enqueue_style() // Intègre le link:css dans la page
@@ -21,26 +21,30 @@ wp_enqueue_scripts // le hook qui permettra d'enfiler le css et le script
 
 function enfiler_script_css()
 {
-    $version_css = filemtime(plugin_dir_path(__FILE__) . 'style.css');
-    $version_js = filemtime(plugin_dir_path(__FILE__) . 'js/carrousel.js');
-    wp_enqueue_style('style_carrousel', plugin_dir_url(__FILE__) . 'style.css', 
-        array(), 
-        $version_css);
-    wp_enqueue_script('js_carrousel', plugin_dir_url(__FILE__) . 'js/carrousel.js',
-        array(), 
-        $version_js, 
-        true);
+   $version_css =  filemtime(plugin_dir_path(__FILE__) . 'style.css');
+   $version_js = filemtime(plugin_dir_path(__FILE__) . 'js/carrousel.js');
+   wp_enqueue_style('style_carrousel',
+        plugin_dir_url(__FILE__) . 'style.css',
+        array(),
+        $version_css
+);
+    wp_enqueue_script('js_carrousel',
+            plugin_dir_url(__FILE__) . 'js/carrousel.js',
+            array(),
+            $version_js,
+            true
+    );
+
 }
+add_action('wp_enqueue_scripts', 'enfiler_script_css' );
 
-add_action('wp_enqueue_scripts', 'enfiler_script_css');
-
-function generer_boite() {
+function genere_boite()
+{
     return '<button class="carrousel__ouvrir">Ouvrir Carrousel</button>
             <div class="carrousel">
                 <button class="carrousel__x">X</button>
-                <figure class-"carrousel__figure"></figure>
+                <figure class="carrousel__figure"></figure>
                 <form class="carrousel__form"></form>
-            </div>
-    ';
+            </div>';
 }
-add_shortcode('carrousel', 'generer_boite');
+add_shortcode('carrousel', 'genere_boite');
